87c6cecaf9600071db5be2424aabd2b808775c56
heronoa <heron.amaral@gmail.com>
2024-05-30 01:24:11 -0300

    refactor: debt calculation functions

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -17,10 +17,9 @@ export const timestampFromNow = ({
   weeks?: number;
   months?: number;
   years?: number;
-  initial_date?: Date
-
+  initial_date?: Date;
 }) => {
-  const currentDate =  initial_date ? new Date(initial_date) : new Date();
+  const currentDate = initial_date ? new Date(initial_date) : new Date();
 
   currentDate.setDate(currentDate.getDate() + (days + weeks * 7));
   currentDate.setHours(currentDate.getHours() + hours);
@@ -56,7 +55,38 @@ export async function getTime() {
   const time = await axios.get(
     "http://worldtimeapi.org/api/timezone/America/Belem",
   );
-  console.log({ time });
 
   return time.data;
+}
+
+export function dataMaisProximaHoje(
+  dates: Date[],
+): { data: Date; posicao: number } | null {
+  if (dates.length === 0) {
+    return null;
+  }
+
+  const hoje = new Date();
+  if (hoje.getTime() < new Date(dates[0]).getTime()) {
+    return null;
+  }
+  let menorDiferenca = Math.abs(hoje.getTime() - new Date(dates[0]).getTime());
+  let posicao = 0;
+
+  for (let i = 1; i < dates.length; i++) {
+    const diferenca = Math.abs(hoje.getTime() - new Date(dates[i]).getTime());
+    if (
+      hoje.getTime() < new Date(dates[i]).getTime() ||
+      diferenca > menorDiferenca
+    ) {
+      break;
+    }
+    menorDiferenca = diferenca;
+    posicao = i;
+  }
+
+  return {
+    data: dates[posicao],
+    posicao,
+  };
 }
